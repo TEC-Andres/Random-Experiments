1e0335b2b6fe53f11746ff823069c9c8f13414cf
Andrés Rodríguez Cantú <a01287002@tec.mx>
2025-01-16 19:53:07 -0600

    Update main.py

--- Luis Enrique Extended Essay Code/main.py
+++ Luis Enrique Extended Essay Code/main.py
@@ -4,7 +4,7 @@
 #
 #       Copyright (C) Tecnologico de Monterrey
 #
-#       File: main.py
+#       Archivo: main.py
 #
 #       Created on:                  16/01/2024
 #       Last edited on:              16/01/2024
@@ -13,21 +13,19 @@ import time
 start_time = time.time()
 
 # Variable control
-index_list= 100_000 # O(n^2) time complexity, reduce the index list to have faster results
-N = 1000
+index_list = 100_000_000  # O(n) time complexity
+N = 1234
 G = 2
 
-# List 1
-data_list = []
-for i in range(1, index_list+1):
-    data_list.append(G ** i)
-
+# G ** R % N
 def main():
-    for i, data in enumerate(data_list):
-        if data % N == 1:
-            print(f"Found MOD {data % N} at r = {i+1}")
+    data = 1
+    for i in range(1, index_list + 1):
+        data = (data * G) % N
+        if data == 1:
+            print(f"Found MOD {data} at r = {i}")
             break
-        elif i == len(data_list)-1:
+        elif i == index_list:
             print(f"No MOD found with {index_list} iterations")
             return
 
